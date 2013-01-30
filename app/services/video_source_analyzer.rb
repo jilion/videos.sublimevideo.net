e4f1d359541426bbf5531225491a7fb4289e53fe
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-01-30 11:46:51 +0100

    Add VideoSourceAnalyzer

---
+++ app/services/video_source_analyzer.rb
@@ -0,0 +1,31 @@
+class VideoSourceAnalyzer
+  VIMEO_URL_PART = 'player.vimeo.com/external'
+
+  attr_reader :source
+
+  def initialize(source)
+    @source = source
+  end
+
+  def origin
+    @origin ||= case url
+    when %r{#{VIMEO_URL_PART}} then 'vimeo'
+    else
+      'other'
+    end
+  end
+
+  def id
+    @id ||= case origin
+    when 'vimeo' then url.match(%r{//#{VIMEO_URL_PART}/(\d+)\..*})[1]
+    else
+      nil
+    end
+  end
+
+  private
+
+  def url
+    source.url
+  end
+end
---
+++ spec/services/video_source_analyzer_spec.rb
@@ -0,0 +1,21 @@
+require 'fast_spec_helper'
+require 'services/video_source_analyzer'
+
+describe VideoSourceAnalyzer do
+  let(:other_video_source) { OpenStruct.new(url: 'http://standard.com/video.mp4') }
+  let(:vimeo_video_source) { OpenStruct.new(url: 'http://player.vimeo.com/external/49154845.sd.mp4?s=f10c9e0acaf7cb38e9a5539c6fbcb4ac') }
+
+  context "with normal video sources" do
+    subject { described_class.new(other_video_source) }
+
+    its(:origin) { should eq 'other' }
+    its(:id) { should be_nil }
+  end
+
+  context "with Vimeo video sources" do
+    subject { described_class.new(vimeo_video_source) }
+
+    its(:origin) { should eq 'vimeo' }
+    its(:id) { should eq '49154845' }
+  end
+end
