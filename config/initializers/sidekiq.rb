f7139d9d0f2684171cc24e34f51d017f38c6fcb2
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-04-15 08:53:33 +0200

    Increase ActiveRecord pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@ require 'sidekiq'
 
 Sidekiq.configure_server do |config|
   if database_url = ENV['DATABASE_URL']
-    ENV['DATABASE_URL'] = "#{database_url}?pool=27"
+    ENV['DATABASE_URL'] = "#{database_url}?pool=30"
     ActiveRecord::Base.establish_connection
   end
 end
