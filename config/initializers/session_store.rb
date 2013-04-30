66c5979e9e7598f715d5d09050252fd9f513ee25
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-04-30 08:53:02 +0200

    Update to Rails 4.0.0.rc1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-VideosSublimeVideo::Application.config.session_store :encrypted_cookie_store, key: '_videos_sublimevideo_session'
+VideosSublimeVideo::Application.config.session_store :cookie_store, key: '_videos_sublimevideo_session'
