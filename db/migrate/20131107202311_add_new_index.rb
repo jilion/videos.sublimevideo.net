a9163660022ac86086d728d7fcaddc6045a079a0
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-11-07 21:27:38 +0100

    Add new index

---
+++ db/migrate/20131107202311_add_new_index.rb
@@ -0,0 +1,7 @@
+class AddNewIndex < ActiveRecord::Migration
+  def change
+    remove_index :video_tags, :starts_updated_at
+
+    add_index :video_tags, [:site_token, :starts_updated_at, :started_at], order: { started_at: :desc }
+  end
+end
