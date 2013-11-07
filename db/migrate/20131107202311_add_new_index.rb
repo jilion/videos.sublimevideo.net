8ad1d072cc1a3daaa4c096b87543b52e95cdebdd
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-11-07 21:32:34 +0100

    Add a name to the index

--- db/migrate/20131107202311_add_new_index.rb
+++ db/migrate/20131107202311_add_new_index.rb
@@ -2,6 +2,6 @@ class AddNewIndex < ActiveRecord::Migration
   def change
     remove_index :video_tags, :starts_updated_at
 
-    add_index :video_tags, [:site_token, :starts_updated_at, :started_at], order: { started_at: :desc }
+    add_index :video_tags, [:site_token, :starts_updated_at, :started_at], order: { started_at: :desc }, name: 'index_update_starts'
   end
 end
