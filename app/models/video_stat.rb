50f2f2cd2a7b49986ad9038bbe60117a75dce384
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-11-18 11:35:53 +0100

    Fix https://www.honeybadger.io/inspector/p/1632/2313542

--- app/models/video_stat.rb
+++ app/models/video_stat.rb
@@ -6,7 +6,9 @@ class VideoStat
   collection_path '/private_api/video_stats'
 
   def self.last_days_starts(video_tag, days)
-    result = get_raw(:last_days_starts, site_token: video_tag.site_token, video_uid: video_tag.uid, days: days)
-    result[:parsed_data][:data]
+    rescue_and_retry(3) do
+      result = get_raw(:last_days_starts, site_token: video_tag.site_token, video_uid: video_tag.uid, days: days)
+      result[:parsed_data][:data]
+    end
   end
 end
